8f47da01aa85b5223338089e4f8d2fe07245a00f
XTeamXeno <messmeraugustus@gmail.com>
2021-07-08 19:19:35 -0500

    alpha 3.0.0

--- nim.py
+++ nim.py
@@ -1,68 +1,19 @@
 import random
 import time
+import os
 
 coins_left = 12  # Number of total coins left
-
-
-def players_turn(total):  # define players choices and total will equal coins_left later in the code
-    take_away = 0
-    loop = 1
-    print("Now its your turn!")
-    time.sleep(0.2)
-    while loop == 1:
-        loop = 0
-        take_away = int(input("How many coins would you like to take away? (1/2/3)"))
-        # Number of coins to subtract from the total
+active = True
+while active:
+    start = input("Do you want to play nim? (y/n)")
+    if start == "y":
+        print("OK! Starting game")
         time.sleep(0.2)
-        if take_away > total:  # Too many
-            print("You took to many!")
-            loop = 1
-        if take_away > 3:  # Too many
-            print("You took to many!")
-            loop = 1
-        if take_away < 1:  # Too few
-            print("You took to few!")
-            loop = 1
-    print("Ok!")
-    time.sleep(0.2)
-    total = total - take_away
-    return total  # return amount left as total
-
-
-def computers_turn(total):  # define computers action
-    print("Now its the computers turn!")
-    time.sleep(0.2)
-    subtract = 0
-    if total > 3:
-        subtract = random.randint(1, 3)  # randomly choose
-    if total <= 3:
-        subtract = total  # win if possible
-    total = total-subtract
-    return total  # return amount left as total
-
-
-start = input("Do you want to play nim? (y/n)")  # ask if they want to play
-if start == "y":
-    print("Ok!")
-    time.sleep(0.2)
-    while coins_left > 0:
-        coins_left = players_turn(coins_left)  # do players turn
-        print("There are now", coins_left, "amount of coins left!")
-        if coins_left == 0:  # win for player
-            print("Congrats! You win!")
-            time.sleep(0.5)
-            exit("win")
-        coins_left = computers_turn(coins_left)
-        print("There are now", coins_left, "amount of coins left!")
-        if coins_left == 0:  # win for computer
-            print("Aww man! The computer won!")
-            time.sleep(0.5)
-            exit("lose")
-elif start == "n":
-    print("Ok!")
-    time.sleep(0.5)
-    exit("responded n to start")
-else:
-    print("Invalid answer!")
-    time.sleep(0.5)
-    exit("invalid response to start")
+        active = False
+    elif start == "n":
+        print("OK! Ending program")
+        input("(Press enter to exit)")
+        exit()
+    else:
+        print("You entered an incorrect string! Try again please!")
+time.sleep(0.5)
